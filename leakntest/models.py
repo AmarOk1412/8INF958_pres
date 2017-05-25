6d876873190cffa6dda90c304be04ed2f657188d
Aburam <baptiste.buron@gmail.com>
2017-05-25 18:18:44 -0400

    fix

--- leakntest/models.py
+++ leakntest/models.py
@@ -6,7 +6,7 @@ class Entry(models.Model):
     name = models.CharField(max_length=100)
     mail = models.CharField(max_length=100)
     password = models.CharField(max_length=255)
-    hash = models.CharField(max_length=255)
+    hashword = models.CharField(max_length=255)
     website = models.CharField(max_length=100)
 
     def __str__(self):
